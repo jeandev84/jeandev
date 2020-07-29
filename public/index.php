8b301d7d3224542aab69917fa4c1bc2709e84c67
Yao Jean-Claude <jeandev84@yahoo.com>
2020-07-29 17:36:22 +0300

    add any route

--- public/index.php
+++ public/index.php
@@ -79,6 +79,9 @@ $container->calls();
 
 $router = new Router();
 
+
+
+/*
 $options = [
     'prefix' => '/admin',
     'namespace' => 'Admin\\'
@@ -100,8 +103,15 @@ $router->group($options, function ($router) {
 
 });
 
+*/
+
+$router->map('GET', '/:any', 'HomeController@index', 'home');
 
 dump($router->getNamedRoutes());
 dump($router->getRoutes());
 
+
+$route = $router->match($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']);
+
+dump($route);
 dd($container);
